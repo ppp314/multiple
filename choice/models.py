efb92a2af42a704943c50c5e30af75e01039d9f2
37033555+ppp314 <37033555+ppp314@users.noreply.github.com>
2019-02-15 20:25:45 +0900

    The first model implement

--- choice/models.py
+++ choice/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from multiselectfield import MultiSelectField
+from django.utils import timezone
+# -*- coding:utf-8 mode:python -*-
 
 # Create your models here.
 MY_CHOICES = (('item_key1', 'Item title 1.1'),
@@ -13,6 +15,21 @@ MY_CHOICES2 = ((1, 'Item title 2.1'),
                (3, 'Item title 2.3'),
                (4, 'Item title 2.4'),
                (5, 'Item title 2.5'))
+
+
+class MyExam(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    created_date = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.title
+
+class MyChoice(models.Model):
+    Exam = models.ForeignKey('MyExam', on_delete=models.CASCADE)
+    no = models.IntegerField(default=0)
+    my_choice = MultiSelectField(choices=MY_CHOICES2,max_choices=1, max_length=6)
+    
 
 class MyModel(models.Model):
 
