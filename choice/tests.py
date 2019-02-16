7167fcae24b53b10d2f72f15719912d201d08da1
37033555+ppp314 <37033555+ppp314@users.noreply.github.com>
2019-02-16 21:22:30 +0900

    Put my first tests.py

--- choice/tests.py
+++ choice/tests.py
@@ -1,3 +1,41 @@
+"""
+Copyright 2019 Akashia Shop
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+from .models import MyExam
+
+def create_exam(exam_auther, exam_title):
+    """
+    Create a exam with tthe given exam_author and exam_title.
+    """
+    return Myexam.objects.create(author=exam_author, title=exam_title)
+
+
+class ExamIndexViews(TestCase):
+    def test_no_exam(self):
+        # If no exam exists, an approperiate messages is to be displayed
+        response = self.client.get(reverse('choice:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains("No exam is available")
+        self.assertQuerysetEqual(response.context['latest_exam_list'], [])
+    
+
+
+
