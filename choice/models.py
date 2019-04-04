e8c9f26e50d58ff8ee27bf48f085006b2386f288
37033555+ppp314 <37033555+ppp314@users.noreply.github.com>
2019-04-05 05:47:08 +0900

    Merge from master to update License.

--- choice/models.py
+++ choice/models.py
@@ -1,4 +1,5 @@
 '''
+<<<<<<< HEAD
     Multiple is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -8,6 +9,25 @@
     along with Multiple.  If not, see <https://www.gnu.org/licenses/>.
 '''
 
+=======
+Copyright 2019 Acacia Shop
+
+This file is part of multiple.
+
+    Multiple is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Multiple is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+'''
+>>>>>>> master
 
 from django.db import models
 from django.urls import reverse
