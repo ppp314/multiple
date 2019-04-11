5832dd0d53466f357e74369b24ec67785f37746e
37033555+ppp314 <37033555+ppp314@users.noreply.github.com>
2019-04-11 23:16:37 +0900

    Remove the broken test and Add post test.
    Remove the function test_file_formset_update.
    Add test_add_post using data set for formset.

--- choice/test_views.py
+++ choice/test_views.py
@@ -42,11 +42,15 @@ class InlineViewTest(TestCase):
         self.assertContains(response, 'INITIAL_FORMS')
         self.assertContains(response, 'TOTAL_FORMS')
 
-    def test_file_formset_update(self):
-        url = reverse('choice:update_post')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'INITIAL_FORMS')
-        self.assertContains(response, 'TOTAL_FORMS')
-
-        
+    def test_add_post(self):
+        """ test if calling post on inline formset results in redirect"""
+        data = {'title': "TESTTITLE",
+                'url': "TESTURL",
+                'file_set-TOTAL_FORMS': "5",
+                'file_set-INITIAL_FORMS': "0",
+                'file_set-MIN_NUM_FORMS': "0",
+                'file_set-MAX_NUM_FORMS': "5"
+                }
+        url = reverse('choice:add-post')
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 302)
