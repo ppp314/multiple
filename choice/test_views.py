b87f21951bcfbe682298b9f81f3d7328a6176e1b
37033555+ppp314 <37033555+ppp314@users.noreply.github.com>
2019-04-12 23:16:25 +0900

    Add test for addquestion.
    Upon adding, there are a few tests which no longer work well. Fix them.

--- choice/test_views.py
+++ choice/test_views.py
@@ -19,6 +19,7 @@ This file is part of Multiple.
 
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 class FormSetCreateViewTest(TestCase):
@@ -29,6 +30,36 @@ class FormSetCreateViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'INITIAL_FORMS')
         self.assertContains(response, 'TOTAL_FORMS')
+
+
+class ExaamQuestionInlineViewTest(TestCase):
+    """
+        Test inlineformset_factory.
+    """
+    def test_get_exam_quesion_inline(self):
+        """ Test method==GET"""
+        url = reverse('choice:add-question')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'question_set-TOTAL_FORMS')
+        self.assertContains(response, 'question_set-INITIAL_FORMS')
+        self.assertContains(response, 'question_set-MIN_NUM_FORMS')
+        self.assertContains(response, 'question_set-MAX_NUM_FORMS')
+
+    def test_post_exam_question_inline(self):
+        """ Test method==POST"""
+        author = User.objects.get_or_create(username='ss')[0]
+
+        data = {'author': author,
+                'Title': "TESTURL",
+                'question_set-TOTAL_FORMS': "5",
+                'question_set-INITIAL_FORMS': "0",
+                'question_set-MIN_NUM_FORMS': "0",
+                'question_set-MAX_NUM_FORMS': "5"
+                }
+        url = reverse('choice:add-question')
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 200)
 
 
 class InlineViewTest(TestCase):
