0c9ad6abbacc818f6271f48cc45f0a0cb2bad770
37033555+ppp314 <37033555+ppp314@users.noreply.github.com>
2019-06-13 14:38:36 +0900

    Checkpoint 2.  Start refactoring.

--- choice/urls.py
+++ choice/urls.py
@@ -18,27 +18,15 @@ This file is part of Multiple.
 """
 
 from django.urls import include, path
-
-
-from .views import HomeView, AboutView, \
-    ExamListView, \
-    ExamDeleteView, ExamUpdateView, \
-    ExamCreateView, \
-    QuestionIndexView, \
-    ExamQuestionView, vote, multiple_question_form, EditQuestionView, \
-    ExamDrillListView, \
-    ExamDrillListView4, \
+from .views import HomeView, \
+    AboutView, \
     SuccessView, \
-    AnswerModelFormSetView, \
-    AnswerDeleteView, \
-    MarkUpdateWithInlinesView, \
-    MarkDeleteView
-
-from .views import \
-    DrillCreateView, \
-    DrillUpdateWithInlinesView, \
-    DrillDeleteView
-
+    ExamListView, \
+    ExamCreateView, \
+    ExamUpdateView, \
+    DrillUpdateView, \
+    DrillListView, \
+    MarkUpdateView
 
 app_name = 'choice'
 
@@ -49,126 +37,39 @@ exam_extra_patterns = [
         name='exam-list',
     ),
     path(
-        '<int:pk>/',
-        ExamDetailView.as_view(),
-        name='exam-detail',
-    ),
-    path(
         '<int:pk>/update',
         ExamUpdateView.as_view(),
         name='exam-update'
     ),
     path(
-        '<int:pk>/delete',
-        ExamDeleteView.as_view(),
-        name='exam-delete'
+        '<int:pk>/drillupdate',
+        DrillUpdateView.as_view(),
+        name='drill-update'
+    ),
+    path(
+        '<int:pk>/drill',
+        DrillListView.as_view(),
+        name='drill-list'
     ),
     path(
         'create/',
         ExamCreateView.as_view(),
         name='exam-create'
     ),
-    path(
-        '<int:pk>/drill',
-        ExamDrillListView.as_view(),
-        name='exam-drill-list'
-    ),
-    path(
-        '<int:pk>/drill4',
-        ExamDrillListView4.as_view(),
-        name='exam-drill-list4'
-    ),
-]
-
-answer_extra_patterns = [
-    path(
-        '<int:pk>/',
-        AnswerModelFormSetView.as_view(),
-        name='answer-list',
-    ),
-    path(
-        '<int:pk>/update',
-        AnswerModelFormSetView.as_view(),
-        name='answer-update'
-    ),
-    path(
-        '<int:pk>/delete',
-        AnswerDeleteView.as_view(),
-        name='answer-delete'
-    ),
 ]
 
 drill_extra_patterns = [
     path(
-        '<int:pk>/create',
-        DrillCreateView.as_view(),
-        name='drill-create',
-    ),
-    path(
         '<int:pk>/',
-        DrillUpdateWithInlinesView.as_view(),
-        name='drill-list',
-    ),
-    path(
-        '<int:pk>/update',
-        DrillUpdateWithInlinesView.as_view(),
-        name='drill-update'
-    ),
-    path(
-        '<int:pk>/delete',
-        DrillDeleteView.as_view(),
-        name='drill-delete'
+        MarkUpdateView.as_view(),
+        name='mark-update',
     ),
 ]
-
-mark_extra_patterns = [
-    path(
-        '<int:pk>/',
-        DrillUpdateWithInlinesView.as_view(),
-        name='drill-list',
-    ),
-    path(
-        '<int:pk>/update',
-        DrillUpdateWithInlinesView.as_view(),
-        name='drill-update'
-    ),
-    path(
-        '<int:pk>/delete',
-        DrillDeleteView.as_view(),
-        name='drill-delete'
-    ),
-]
-
-mark_extra_patterns = [
-    path(
-        '<int:pk>/',
-        MarkUpdateWithInlinesView.as_view(),
-        name='mark-list',
-    ),
-    path(
-        '<int:pk>/update',
-        MarkUpdateWithInlinesView.as_view(),
-        name='mark-update'
-    ),
-    path(
-        '<int:pk>/delete',
-        MarkDeleteView.as_view(),
-        name='mark-delete'
-    ),
-]
-
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('about/', AboutView.as_view(), name='about'),
-    path('exam/', include(exam_extra_patterns)),
-    path('answer/', include(answer_extra_patterns)),
-    path('drill/', include(drill_extra_patterns)),
-    path('mark/', include(mark_extra_patterns)),
-    path('p/<int:pk>/', QuestionIndexView.as_view(), name='question-index'),
-    path('vote/<int:pk>/', vote, name='exam-vote'),
-    path('testform/', multiple_question_form, name='test-form'),
-    path('editquestion/<int:pk>', EditQuestionView.as_view(),
-         name='edit-question'),
     path('success/', SuccessView.as_view(), name='success'),
+    path('exam/', include(exam_extra_patterns)),
+    path('drill/', include(drill_extra_patterns)),
 ]
